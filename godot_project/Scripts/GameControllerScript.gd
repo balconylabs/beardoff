30e314f0c5a09b4d1f358a6343131cb70abf57a3
Aaron Askew <aaronaskew@gmail.com>
2017-11-18 21:18:53 -0600

    fixed restart, refactored some variables, added some comments

--- godot_project/Scripts/GameControllerScript.gd
+++ godot_project/Scripts/GameControllerScript.gd
@@ -1,17 +1,42 @@
 extends Node2D
+
+#total time elapsed this game
 var elapsedTime = 0
+
+#time the last tick occurred
 var lastTickTime = 0
 
+#total ticks elapsed this game
 var currentTick = 0
-var animTick = .1
-var numTicksAnimSwitch = 12
-var beardVisibleTicks = 11
-var pixelScale = 7.5
 
-var gameRunning = true
+#how long a tick takes
+var secondsPerTick = .1
 
+
+
+#node variables
 var head
 var beard
+var gameOverNode
+
+#scene variables
+var gameOverScene
+
+#number of ticks before the face sprite freezes animation
+var numTicksAnimSwitch = 12
+
+#number of ticks before the long beard sprite is displayed
+var beardVisibleTicks = 11
+
+#scale factor to fill screen with beard display
+var beardDisplayPixelScale = 7.5
+
+#initial position of long beard sprite
+var initBeardPosX = 0
+var initBeardPosY = -404
+ 
+var gameRunning = false
+
 
 
 
@@ -21,34 +46,44 @@ func _ready():
 	head = get_node("headSprite")
 	beard = get_node("longbeard")
 	
+	gameOverScene = load("res://Scenes/GameOverScene.tscn")
+	gameOverNode = gameOverScene.instance()
+	
+	gameRunning = true
 	set_process(true)
 
 func _process(delta):
 	elapsedTime += delta
 	
-	if((elapsedTime - lastTickTime >= animTick) and gameRunning):
-		#do tick
-		#print("tick: ")
-		
-		if(currentTick < numTicksAnimSwitch):
-			head.set_frame(head.get_frame() + 1)
-		else:
-			var pos = beard.get_position()
-			beard.set_position(Vector2(pos.x, pos.y + 1 * pixelScale))
-		
-		if(currentTick == beardVisibleTicks):
-			beard.show()
-		
-		currentTick += 1
-		lastTickTime = elapsedTime
-		
-	if (beard.position.y >= 0):
-		lose()
-		
+	if(gameRunning):
+		if(elapsedTime - lastTickTime >= secondsPerTick):
+			
+			#do tick
+			
+			#if OS.is_debug_build():
+			#	print("tick: ", currentTick)
+						
+			if(currentTick < numTicksAnimSwitch):
+				head.set_frame(head.get_frame() + 1)
+			else:
+				var pos = beard.get_position()
+				beard.set_position(Vector2(pos.x, pos.y + 1 * beardDisplayPixelScale))
+			
+			if(currentTick == beardVisibleTicks):
+				beard.show()
+			
+			currentTick += 1
+			lastTickTime = elapsedTime
+			
+		if (beard.position.y >= 0):
+			lose()
+
+
 func _on_Button_pressed():
+	
 	#shave
 	head.set_frame(0)
-	beard.set_position(Vector2(0, -404))
+	beard.set_position(Vector2(initBeardPosX, initBeardPosY))
 	beard.hide()
 	currentTick = 0
 	
@@ -59,16 +94,28 @@ func lose():
 	
 	#display "game over"
 	#show restart option
-	var gameOverScene = load("res://Scenes/GameOverScene.tscn")
-	var gameOverNode = gameOverScene.instance()
+
 	var pos = Vector2(24 * 7.5, 0);
 	gameOverNode.set_position(pos)
 	add_child(gameOverNode)
 	
 func restart():
-	head.hide()
-	pass
+	#restart the game
+	
+	#reset game variables
+	elapsedTime = 0
+	lastTickTime = 0
+	currentTick = 0
+	
+	#reset beard display
+	head.set_frame(0)
+	beard.set_position(Vector2(initBeardPosX, initBeardPosY))
+	beard.hide()
 
-
-
+	#reset the game board
+	
+	remove_child(gameOverNode)
+	
+	#start game
+	gameRunning = true
 
