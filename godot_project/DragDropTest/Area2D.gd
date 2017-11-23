fb0d9809a93f190f574acf9f7de9b55ab57b2e1e
Aaron Askew <aaronaskew@gmail.com>
2017-11-23 10:01:57 -0600

    added some more drag/drop logic

--- godot_project/DragDropTest/Area2D.gd
+++ godot_project/DragDropTest/Area2D.gd
@@ -4,12 +4,16 @@ extends Area2D
 # var a = 2
 # var b = "textvar"
 
-var isDragging = false
+export var isDragging = false
 
 var mouse_tile_pos_offset
 
+var original_position
+
 var tile_node
 var tile_size
+
+export var overValidTarget = false
 
 func _ready():
 	# Called every time the node is added to the scene.
@@ -29,14 +33,20 @@ func _input_event(viewport, event, shape_idx):
 		if(event.pressed):
 			print("Clicked")
 			isDragging = true
+			#store original position
+			original_position=position
 			#save mouse offset
 			print(self.position)
 			print(get_global_mouse_position())
 			mouse_tile_pos_offset = self.position - get_global_mouse_position()
 		else:
 			print("Released")
+			print("overValidTarget: ",overValidTarget)
+			if(overValidTarget):
+				position.x = clamp(position.x, tile_size.x/2, get_viewport_rect().size.x - tile_size.x / 2)
+				position.y = clamp(position.y, tile_size.y/2, get_viewport_rect().size.y - tile_size.y / 2)
+			else:
+				position = original_position
 			isDragging = false
-			position.x = clamp(position.x, tile_size.x/2, get_viewport_rect().size.x - tile_size.x / 2)
-			position.y = clamp(position.y, tile_size.y/2, get_viewport_rect().size.y - tile_size.y / 2)
-			
+						
 			
