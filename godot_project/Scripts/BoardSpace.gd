0e8c5eb109516a8fa11c8069619c4ae608b89bcb
Aaron Askew <aaronaskew@gmail.com>
2017-11-24 17:22:52 -0600

    hardened the tile placement and queue as well as the restart

--- godot_project/Scripts/BoardSpace.gd
+++ godot_project/Scripts/BoardSpace.gd
@@ -4,11 +4,23 @@ extends Node2D
 # var a = 2
 # var b = "textvar"
 
+#ref to GameControllerNode
+onready var gc = get_node("/root/gameController")
+
 #neighbors
 var U = null
 var D = null
 var L = null
 var R = null
+
+#is there a tile on this space?
+var isEmpty = true
+
+var isPlug = false
+var isRazor = false
+
+#will point the the tile that is placed on this space
+var wireTile = null
 
 func _ready():
 	# Called every time the node is added to the scene.
@@ -19,3 +31,25 @@ func _ready():
 #	# Called every frame. Delta is time since last frame.
 #	# Update game logic here.
 #	pass
+
+
+func _on_Area2D_mouse_entered():
+	get_node("Area2D/empty_tile_64x64").frame = 1
+	pass # replace with function body
+	
+	
+
+
+func _on_Area2D_mouse_exited():
+	get_node("Area2D/empty_tile_64x64").frame = 0
+	pass # replace with function body
+
+
+func _on_Area2D_input_event( viewport, event, shape_idx ):
+	if(event is InputEventMouseButton && event.pressed):
+		if(event.button_index == BUTTON_LEFT && isEmpty):
+			wireTile = gc.grabNextTile()
+			if(OS.is_debug_build()): print(wireTile)
+			wireTile.set_position(position)
+			isEmpty = false
+
--- godot_project/Scripts/GameControllerScript.gd
+++ godot_project/Scripts/GameControllerScript.gd
@@ -10,7 +10,7 @@ var lastTickTime = 0
 var currentTick = 0
 
 #how long a tick takes
-var secondsPerTick = .1
+var secondsPerTick = .5
 
 #node variables
 var gameOverNode
@@ -65,61 +65,24 @@ enum ConnectionType {
 
 func _ready():
 	
+	
 	gameOverScene = load("res://Scenes/GameOverScene.tscn")
 	gameOverNode = gameOverScene.instance()
 	
 	beardStatusScene = load("res://Scenes/BeardStatusScene.tscn")
 	beardStatusNode = beardStatusScene.instance()
-	add_child(beardStatusNode)
+	add_child(beardStatusNode,true)
 	beardScale = Vector2(beardDisplayPixelScale,beardDisplayPixelScale)
 	beardStatusNode.set_scale(beardScale)
 	
 	#initialize game board
-	
-	boardSpaceScene = preload("res://Scenes/BoardSpace.tscn")
-	
-	gameboard = []
-	
-	for i in range(GB_COLS):
-		if(OS.is_debug_build()): print("i=",i)
-		gameboard.append([])
-		for j in range(GB_ROWS):
-			gameboard[i].append(boardSpaceScene.instance())
-			#gameboard[i][j]=boardSpaceScene.instance()
-			if(OS.is_debug_build()): print(gameboard[i][j])
-				
-			gameboard[i][j].set_position(Vector2(i * TILE_WIDTH + TILE_X_OFFSET + GB_X_OFFSET, j * TILE_HEIGHT + TILE_Y_OFFSET + GB_Y_OFFSET))
-			add_child(gameboard[i][j])
-			
-	#set gameboard neighbors
-	for i in range(GB_COLS):
-		for j in range(GB_ROWS):
-				#set neighbors
-			if(i > 0):
-				gameboard[i][j].L = gameboard[i-1][j]
-			if(i < GB_COLS - 1):
-				gameboard[i][j].R = gameboard[i+1][j]
-			if(j > 0):
-				gameboard[i][j].U = gameboard[i][j-1]
-			if(j < GB_ROWS - 1):
-				gameboard[i][j].D = gameboard[i][j+1]
-				
-			#print the neigbors
-			print("U ",gameboard[i][j].U)
-			print("D ",gameboard[i][j].D)
-			print("L ",gameboard[i][j].L)
-			print("R ",gameboard[i][j].R)
+	initGameBoard()
+
 			
 			
 			
 	#init tile queue
-	tileQueue = []
-	for i in range(QUEUE_LENGTH):
-		tileQueue.push_back(boardSpaceScene.instance())
-		add_child(tileQueue[i])
-		tileQueue[i].set_position(Vector2(TILE_X_OFFSET + QUEUE_X_OFFSET, i * TILE_HEIGHT + TILE_Y_OFFSET + QUEUE_Y_OFFSET))
-		
-		pass
+	initTileQueue()
 			
 	gameRunning = true
 	set_process(true)
@@ -144,6 +107,61 @@ func _process(delta):
 		if(beardStatusNode.isTooLong()):
 			lose()
 
+func initTileQueue():
+	
+	#clear any previous tiles
+	for i in get_node("WireTilesNode").get_children():
+		i.queue_free()
+	
+	tileQueue = []
+	for i in range(QUEUE_LENGTH):
+		#tileQueue.push_back(boardSpaceScene.instance())
+		tileQueue.push_back(randomWireTile())
+		get_node("WireTilesNode").add_child(tileQueue[i],true)
+		tileQueue[i].set_position(Vector2(TILE_X_OFFSET + QUEUE_X_OFFSET, i * TILE_HEIGHT + TILE_Y_OFFSET + QUEUE_Y_OFFSET))
+		
+	if(OS.is_debug_build()): print("init queue: ",tileQueue)
+		
+func initGameBoard():
+	
+	#clear out previous board
+	for i in get_node("GameBoardNode").get_children():
+		i.queue_free()
+	
+	boardSpaceScene = preload("res://Scenes/BoardSpace.tscn")
+	
+	gameboard = []
+	
+	for i in range(GB_COLS):
+#		if(OS.is_debug_build()): print("i=",i)
+		gameboard.append([])
+		for j in range(GB_ROWS):
+			gameboard[i].append(boardSpaceScene.instance())
+			#gameboard[i][j]=boardSpaceScene.instance()
+#			if(OS.is_debug_build()): print(gameboard[i][j])
+				
+			gameboard[i][j].set_position(Vector2(i * TILE_WIDTH + TILE_X_OFFSET + GB_X_OFFSET, j * TILE_HEIGHT + TILE_Y_OFFSET + GB_Y_OFFSET))
+			get_node("GameBoardNode").add_child(gameboard[i][j],true)
+			
+	#set gameboard neighbors
+	for i in range(GB_COLS):
+		for j in range(GB_ROWS):
+				#set neighbors
+			if(i > 0):
+				gameboard[i][j].L = gameboard[i-1][j]
+			if(i < GB_COLS - 1):
+				gameboard[i][j].R = gameboard[i+1][j]
+			if(j > 0):
+				gameboard[i][j].U = gameboard[i][j-1]
+			if(j < GB_ROWS - 1):
+				gameboard[i][j].D = gameboard[i][j+1]
+				
+			#print the neigbors
+#			print("U ",gameboard[i][j].U)
+#			print("D ",gameboard[i][j].D)
+#			print("L ",gameboard[i][j].L)
+#			print("R ",gameboard[i][j].R)
+
 
 func _on_Button_pressed():
 	
@@ -153,18 +171,27 @@ func _on_Button_pressed():
 	
 func lose():
 	
+	
+	
 	#pause everything
 	gameRunning = false
 	
 	#display "game over"
 	#show restart option
 
+	for i in get_node("GameOverScreen").get_children():
+		i.queue_free()
+
 	var pos = Vector2(24 * 7.5, 0);
 	gameOverNode.set_position(pos)
-	add_child(gameOverNode)
+	get_node("GameOverScreen").add_child(gameOverNode,true)
+	
 	
 func restart():
 	#restart the game
+	
+	initGameBoard()
+	initTileQueue()
 	
 	#reset game variables
 	elapsedTime = 0
@@ -172,15 +199,47 @@ func restart():
 	currentTick = 0
 	
 	#reset beard display
-	remove_child(beardStatusNode)
+	beardStatusNode.queue_free()
 	beardStatusNode = beardStatusScene.instance()
-	add_child(beardStatusNode)
+	add_child(beardStatusNode,true)
 	beardStatusNode.set_scale(beardScale)
 
 	#reset the game board
 	
-	remove_child(gameOverNode)
+	get_node("GameOverScreen").remove_child(gameOverNode)
 	
 	#start game
 	gameRunning = true
 
+func randomWireTile():
+	var wireTileNode = preload("res://Scenes/WireTile.tscn").instance()
+	
+	#get random index that will be used to get the frame of the sprite that corresponds to the ConnectionType enum value
+	var tileType = randi() % ConnectionType.size()
+	
+	wireTileNode.connectionType = tileType
+	wireTileNode.get_node("Area2D/wire_tile_sprite").frame = tileType
+	
+	return wireTileNode
+	
+#returns the next tile in the queue, pops it from the queue, adds next tile
+func grabNextTile():
+	var tileToReturn = tileQueue.pop_back()
+#	if(OS.is_debug_build()): print(tileToReturn)
+	
+	
+	enqueueNewTile()
+	
+	
+	return tileToReturn
+
+func enqueueNewTile():
+		#reposition all the tiles in the queue down by 1 tile
+	for tile in tileQueue:
+		tile.set_position(tile.position + Vector2(0,TILE_HEIGHT))
+
+	var newTile = randomWireTile()
+	newTile.set_position(Vector2(TILE_X_OFFSET + QUEUE_X_OFFSET, TILE_Y_OFFSET + QUEUE_Y_OFFSET))
+	tileQueue.push_front(newTile)		
+	
+	get_node("WireTilesNode").add_child(newTile)
